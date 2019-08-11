8c64bb00f627e159ef163caa3b9b6cb32dff5d06
mike-kob <37714867+mike-kob@users.noreply.github.com>
2019-08-11 19:45:50 +0300

    Code style

--- bot/actions.py
+++ bot/actions.py
@@ -3,7 +3,7 @@ from typing import Optional
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from bot import bot
-from settings import CHANNEL_NAME, BOT_URL
+from settings import CHANNEL_NAME
 
 
 def send_to_channel(number_id: int) -> Optional[Message]:
@@ -13,8 +13,7 @@ def send_to_channel(number_id: int) -> Optional[Message]:
     reply_markup = InlineKeyboardMarkup(button_list)
     msg = bot.send_message(chat_id=CHANNEL_NAME,
                            text="Hey, here's new customer struggling to have his belly full "
-                                "of melons. Who's gonna dare to satisfy his appetites?\n"
-                                ":melon::melon::melon:\n"
+                                "of melons. Who's gonna dare to satisfy his appetites?\n\n"
                                 "But remember, that you have to start chatting with this "
                                 "wonderful <a href=\"https://t.me/Regsmthbot\">bot</a> ",
                            reply_markup=reply_markup,
